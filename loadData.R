0784c2848487336f06d44e4b580fcb8cdb403784
Bokharouss <a.k.bokharouss@student.tue.nl>
2017-11-21 14:25:02 +0100

    import new data

--- loadData.R
+++ loadData.R
@@ -1,5 +1,13 @@
-# # load / attach necessary (add-on) packages
+# load / attach necessary (add-on) packages
 library(readr)
 
 applicationEventLogs <- read_csv("./data/applicationEventLog/applicationEvents.csv")
 offerEventLogs <- read_csv("./data/offerEventLog/offerEvents.csv")
+
+applicants <- read.delim("./data/laraData/applicant_20170101-20170930.txt", sep = ';')
+#applicationData <- read.delim("./data/laraData/application_20170101-20170930_anon.txt", sep = ';', dec = ";")
+documents <- read.delim("./data/laraData/document_20170101-20170930.txt", sep = ';')
+incomes <- read.delim("./data/laraData/income_20170101-20170930.txt", sep = ';')
+scenarios <- read.delim("./data/laraData/scenario_20170101-20170930.txt", sep = ';')
+#states <- read.delim("./data/laraData/state_20170101-20170930_anon.txt", sep = ';')
+#workitems <- read.delim("./data/laraData/workitem_20170101-20170930_anon.txt", sep = ';')
