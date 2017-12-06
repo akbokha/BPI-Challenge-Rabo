10603499907fd23bc4a66be55b8adf00e62e2769
Bokharouss <a.k.bokharouss@student.tue.nl>
2017-12-06 16:20:46 +0100

    update loadData file

--- loadData.R
+++ loadData.R
@@ -4,16 +4,13 @@ library(readr)
 applicationEventLogs <- read_csv("./data/applicationEventLog/applicationEvents.csv")
 offerEventLogs <- read_csv("./data/offerEventLog/offerEvents.csv")
 
-applicants <- read.delim("./data/laraData/applicant_20170101-20170930.txt", sep = ';')
-documents <- read.delim("./data/laraData/document_20170101-20170930.txt", sep = ';')
-incomes <- read.delim("./data/laraData/income_20170101-20170930.txt", sep = ';')
-scenarios <- read.delim("./data/laraData/scenario_20170101-20170930.txt", sep = ';')
+applicants <- read_delim("./data/laraData/applicant.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+documents <- read_delim("./data/laraData/document.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+incomes <- read_delim("./data/laraData/income.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+scenarios <- read_delim("./data/laraData/scenario.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 
-applicationDataBadFormat <- read.table("./data/laraData/reformat/application_20170101-20170930.txt", sep = ';')
-#applicationDataBadFormat <- as.data.frame(t(applicationDataBadFormat))
-applicationDataBadFormat <- setNames(data.frame(matrix(unlist(applicationDataBadFormat[-c(1:8),]),
-                                                       ncol = 8, byrow = TRUE)), c(unlist(applicationDataBadFormat[1:8,])))
-
-states <- read.delim("./data/laraData/state_20170101-20170930_anon.txt", sep = ';')
-workitems <- read.delim("./data/laraData/workitem_20170101-20170930_anon.txt", sep = ';')
-offers <- read.delim("./data/laraData/offer_20170101-20170930_anon.txt", sep = ';')
+# new event logs
+application <- read_delim("./data/laraData/application.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+offer <- read_delim("./data/laraData/offer.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+state <- read_delim("./data/laraData/state.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+workitem <- read_delim("./data/laraData/workitem.csv", ";", escape_double = FALSE, trim_ws = TRUE)
